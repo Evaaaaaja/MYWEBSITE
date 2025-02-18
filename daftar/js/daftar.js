046c3e24859204d991a4bc10bc107ec6fb076e38
Ulva Khairunnisya <khairunnisyaulva@gmail.com>
2025-02-18 17:46:27 +0700

    Add files via upload

---
+++ daftar/js/daftar.js
@@ -0,0 +1,8 @@
+function toggleOffcanvas() {
+    const offcanvas = document.getElementById('offcanvas');
+    offcanvas.classList.toggle('active');
+}
+function closeOffcanvas() {
+    const offcanvas = document.getElementById('offcanvas');
+    offcanvas.classList.remove('active');
+}
